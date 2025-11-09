4c9747cfe29025545674413faf1cb1e0b259a02b
Kemal Can <55075575+squeizee@users.noreply.github.com>
2025-11-09 22:59:36 +0100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:'/<Professionalportfoliosite>/',
+    base:'/Professionalportfoliosite/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
