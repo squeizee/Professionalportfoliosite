0ca4abe18e18f20dd308afa1293d8f6c57068a79
Kemal Can <55075575+squeizee@users.noreply.github.com>
2025-11-09 23:50:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Professionalportfoliosite/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
